51e9f471908f137f2908a55f5832205e51f58065
tzetzo <tzvetanmarinov@yahoo.com>
2020-02-18 21:06:16 +0200

    tzetzo feature

--- src/Input.test.js
+++ src/Input.test.js
@@ -27,7 +27,7 @@ const setup = (initialState = {}) => {
     .dive();
   return wrapper;
 };
-
+// tzetzo feature
 // Redux recommended approach:
 // const setup = (props = { success: false }) => {
 //   return shallow(<Input {...props} />);
